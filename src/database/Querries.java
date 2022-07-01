f1d568d846d114f5a0f0ee77c826fe3405761c3a
Lucas Mihara <miharalucas@gmail.com>
2022-07-01 17:43:40 -0300

    fixed pokemon querry

--- src/database/Querries.java
+++ src/database/Querries.java
@@ -2,7 +2,6 @@ package database;
 
 public final class Querries {
     private Querries() { }
-
-    public static String GetPokemon = "SELECT * FROM POKEMON WHERE NUMERO = NVL(?, NUMERO) AND UPPER(NOME) LIKE UPPER(NVL(?, NOME)) AND (UPPER(TIPO_1) = UPPER(NVL(?, TIPO_1)) AND UPPER(TIPO_2) = UPPER(NVL(?, TIPO_2))) OR (UPPER(TIPO_1) = UPPER(NVL(?, TIPO_1)) AND UPPER(TIPO_2) = UPPER(NVL(?, TIPO_2)))";
+    public static String GetPokemon = "SELECT * FROM POKEMON WHERE NUMERO = NVL(?, NUMERO) AND UPPER(NOME) LIKE UPPER(NVL(?, NOME)) AND ((UPPER(TIPO_1) = UPPER(NVL(?, TIPO_1)) AND UPPER(TIPO_2) = UPPER(NVL(?, TIPO_2))) OR (UPPER(TIPO_1) = UPPER(NVL(?, TIPO_1)) AND UPPER(TIPO_2) = UPPER(NVL(?, TIPO_2)))) ORDER BY NUMERO";
     public static String InsertTrainer = "INSERT INTO TREINADOR (CPF, NOME, GENERO, DATA_NASC, CIDADE_NATAL, TITULO) VALUES (?, ?, ?, ?, ?, ?)";
 }
